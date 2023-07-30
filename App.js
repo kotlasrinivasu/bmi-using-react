455de7917cd21a187119ada0cf9884de66dd8ea5
kotlasrinivasu <121429003+kotlasrinivasu@users.noreply.github.com>
2023-07-30 21:45:35 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import BmiCalculator from './components/Bmi';
+
+function App() {
+  return (
+    <div className="App">
+     <BmiCalculator / >
+    </div>
+  );
+}
+
+export default App;
